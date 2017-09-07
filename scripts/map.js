6c11f369b0e98b1b4a9fc5e36fa3f0d9f8643ef1
Viyzed <Andrew@andrews-air.home>
2017-09-07 13:58:24 +0100

    Third Commit
    This will probably break it.

--- scripts/map.js
+++ scripts/map.js
@@ -24,6 +24,14 @@ function initMap() {
   service = new google.maps.places.PlacesService(map);
 }
 
+function nearBySearch() {
+  return service.nearbySearch({
+    location: pos,
+    radius: 4024,
+    type: ['sleep']
+  }, callback);
+}
+
 function getLocation() {
     infoWindow = new google.maps.InfoWindow;
     searchTerm = document.getElementById('search-bar').value;
@@ -35,18 +43,14 @@ function getLocation() {
           lng: position.coords.longitude
         };
 
+        nearBySearch();
+
         infoWindow.setPosition(pos);
-        infoWindow.setContent('You Are Now Here');
+        infoWindow.setContent('You Are Here');
         infoWindow.open(map);
         map.setCenter(pos);
         map.setZoom(16);
         userLocation = pos;
-
-        service.nearbySearch({
-          location: pos,
-          radius: 4024,
-          type: ['sleep']
-        }, callback);
 
         }, function() {
           handleLocationError(true, infoWindow, map.getCenter());
@@ -59,56 +63,55 @@ function getLocation() {
         geocoder = new google.maps.Geocoder();
         geocodeAddress(geocoder, map);
     }
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      infoWindow.setPosition(pos);
-      infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.');
-      infoWindow.open(map);
-    }
-
-    function callback(results, status) {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          createMarker(results[i]);
-          markers += JSON.stringify(results[i]);
-        }
-      }
-      var lItems = $("#menu li");
-      lItems.each(function(li) {
-          console.log($(this).attr('id'));
-        });
-    }
-
-    function createMarker(place) {
-      var placeLoc = place.geometry.location;
-      var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-      });
-
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-      });
-    }
-
-    function geocodeAddress(geocoder, resultsMap) {
-       geocoder.geocode({'address': searchTerm}, function(results, status) {
-         if (status === 'OK') {
-           resultsMap.setCenter(results[0].geometry.location);
-           resultsMap.setZoom(16);
-           infoWindow.setPosition(results[0].geometry.location);
-           infoWindow.setContent('You Are Here');
-           infoWindow.open(resultsMap);
-
-
-
-         } else {
-           alert('The Post Code is not formatted properly. Please enter in the following format: \'AA11 1AA\'');
-         }
-       });
-     }
-
   }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
+
+  function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+        markers += JSON.stringify(results[i]);
+      }
+    }
+    var lItems = $("#menu li");
+    lItems.each(function(li) {
+        console.log($(this).attr('id'));
+      });
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+
+  function geocodeAddress(geocoder, resultsMap) {
+     geocoder.geocode({'address': searchTerm}, function(results, status) {
+       if (status === 'OK') {
+         resultsMap.setCenter(results[0].geometry.location);
+         resultsMap.setZoom(16);
+         infoWindow.setPosition(results[0].geometry.location);
+         infoWindow.setContent('You Are Here');
+         infoWindow.open(resultsMap);
+
+
+
+       } else {
+         alert('The Post Code is not formatted properly. Please enter in the following format: \'AA11 1AA\'');
+       }
+     });
+   }
